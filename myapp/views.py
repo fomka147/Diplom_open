a4267499f991544d40f553ed34a4c38c3e4ac084
fomka147 <33258750+fomka147@users.noreply.github.com>
2025-04-29 19:03:26 +0500

    new

--- myapp/views.py
+++ myapp/views.py
@@ -1,12 +1,12 @@
 from django.views.generic import TemplateView, ListView, DetailView
 from .models import CADSystem, Article
+from django.db.models import Q
 
 class IndexView(TemplateView):
     template_name = 'myapp/index.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # Выбираем 4 популярных САПР (например, по цене или категории)
         context['popular_cads'] = CADSystem.objects.all()[:4]
         return context
 
@@ -18,8 +18,22 @@ class CADSystemListView(ListView):
     def get_queryset(self):
         queryset = super().get_queryset()
         category = self.request.GET.get('category')
+        query = self.request.GET.get('q')
+        price_min = self.request.GET.get('price_min')
+        price_max = self.request.GET.get('price_max')
+        manufacturer = self.request.GET.get('manufacturer')
+
         if category:
             queryset = queryset.filter(category=category)
+        if query:
+            queryset = queryset.filter(name__icontains=query)
+        if price_min:
+            queryset = queryset.filter(price__gte=price_min)
+        if price_max:
+            queryset = queryset.filter(price__lte=price_max)
+        if manufacturer:
+            queryset = queryset.filter(manufacturer__iexact=manufacturer)
+
         return queryset
 
     def get_context_data(self, **kwargs):
@@ -30,6 +44,7 @@ class CADSystemListView(ListView):
             ('cnc', 'ЧПУ'),
             ('other', 'Другое'),
         ]
+        context['manufacturers'] = CADSystem.objects.values('manufacturer').distinct()
         return context
 
 class CADSystemDetailView(DetailView):
@@ -52,13 +67,3 @@ class ArticleDetailView(DetailView):
     model = Article
     template_name = 'myapp/article_detail.html'
     context_object_name = 'article'
-
-def get_queryset(self):
-    queryset = super().get_queryset()
-    category = self.request.GET.get('category')
-    query = self.request.GET.get('q')
-    if category:
-        queryset = queryset.filter(category=category)
-    if query:
-        queryset = queryset.filter(name__icontains=query)
-    return queryset
